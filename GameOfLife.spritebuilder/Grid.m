fd6edeae683044a2d382486e4a7e0f2336de5faf
cinthiarosa <CINTHIAROSA.rj@gmail.com>
2014-07-03 20:34:48 -0300

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -29,6 +29,28 @@ static const int GRID_COLUMNS = 10;
     self.userInteractionEnabled = YES;
 }
 
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    //get the x,y coordinates of the touch
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    //get the Creature at that location
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    
+    //invert it's state - kill it if it's alive, bring it to life if it's dead.
+    creature.isAlive = !creature.isAlive;
+}
+
+- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
+{
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    // Divide the y coordinate of the touch (accessed as touchPosition.y) by the cellHeight to get the row that was touched. Store that value in an integer called row. Divide the x coordinate of the touch (accessed as touchPosition.x) by the cellWidth to get the column that was touched. Store that value in an integer called column.
+    int row = touchPosition.y / _cellHeight;
+    int collumn = touchPosition.x / _cellWidth;
+    return _gridArray[row][collumn];
+
+}
+
 - (void)setupGrid
 {
     // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
@@ -56,8 +78,6 @@ static const int GRID_COLUMNS = 10;
             // this is shorthand to access an array inside an array
             _gridArray[i][j] = creature;
             
-            // make creatures visible to test this method, remove this once we know we have filled the grid properly
-            creature.isAlive = YES;
             
             x+=_cellWidth;
         }
@@ -65,4 +85,5 @@ static const int GRID_COLUMNS = 10;
         y += _cellHeight;
     }
 }
+
 @end
